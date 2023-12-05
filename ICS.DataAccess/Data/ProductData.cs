6fbb1bd480d0e6cd83b3f4786b2c158dc496559a
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2023-12-04 22:42:15 -0300

    Read and ReadAll methods for Product

---
+++ ICS.DataAccess/Data/ProductData.cs
@@ -0,0 +1,19 @@
+﻿using ICS.DataAccess.DbAccess;
+using ICS.DataAccess.Models;
+
+namespace ICS.DataAccess.Data;
+
+public class ProductData(ISqlDataAccess db)
+{
+    public Task<IEnumerable<ProductModel>> GetProducts()
+    {
+        return db.LoadData<ProductModel, dynamic>("dbo.spProduct_ReadAll", new { });
+    }
+
+    public async Task<ProductModel?> GetProduct(string productId)
+    {
+        var result =
+            await db.LoadData<ProductModel, dynamic>("dbo.spProduct_Read", new { ProductId = productId });
+        return result.FirstOrDefault();
+    }
+}
