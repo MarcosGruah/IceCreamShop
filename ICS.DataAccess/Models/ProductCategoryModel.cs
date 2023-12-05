afb029aa6d00f5dab043e9639b2106a0dfa42553
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2023-12-04 22:41:09 -0300

    DataAccess Product and ProductCategory Models

---
+++ ICS.DataAccess/Models/ProductCategoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ICS.DataAccess.Models;
+
+public class ProductCategory
+{
+    public required Guid ProductCategoryId { get; set; }
+    public required string Name { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required DateTime UpdatedAt { get; set; }
+}
---
+++ ICS.DataAccess/Models/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ICS.DataAccess.Models;
+
+public class ProductModel
+{
+    public required Guid ProductId { get; set; }
+    public required Guid ProductCategoryId { get; set; }
+    public required string Name { get; set; }
+    public required int Quantity { get; set; }
+    public required decimal Value { get; set; }
+    public required string Unit { get; set; }
+    public required decimal Price { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required DateTime UpdatedAt { get; set; }
+}
